dc075f42f9009263b9ae34d9f07fd4b0cb3cd34f
Ronald Pogado <pogado.ronald@gmail.com>
2026-08-27 17:04:11 +0800

    Added typescript types

---
+++ src/types/application.ts
@@ -0,0 +1,29 @@
+export type ApplicationStatus = 
+    | 'Applied'
+    | 'Interview'
+    | 'Offer'
+    | 'Rejected'
+    | 'Withdrawn'
+
+export type EmploymentType =
+    | 'Full-time'
+    | 'Part-time'
+    | 'Contract'
+    | 'Internship'
+
+export interface ApplicationFormData {
+    company: string
+    position: string
+    location: string
+    status: ApplicationStatus
+    employmentType: EmploymentType
+    dateApplied: string
+    jobLink: string
+    salary?: number
+    notes: string
+}
+
+export interface Application extends ApplicationFormData{
+    id:string
+    createdAt: string
+}
