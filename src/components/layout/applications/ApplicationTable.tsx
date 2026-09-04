a2e895e8870bd0493e88c85c750b589259e931c9
Ronald Pogado <pogado.ronald@gmail.com>
2026-09-05 02:02:05 +0800

    Added date formatter and view details button

--- src/components/layout/applications/ApplicationTable.tsx
+++ src/components/layout/applications/ApplicationTable.tsx
@@ -1,6 +1,8 @@
 import type { Application } from "../../../types/application";
 
-import { LuPencil, LuTrash2 } from "react-icons/lu";
+import formatDate from "../../../lib/formatDate";
+
+import { LuEye, LuPencil, LuTrash2 } from "react-icons/lu";
 
 type ApplicationTableProps = {
     applications: Application[];
@@ -18,7 +20,9 @@ export default function ApplicationTable({
                 <tr>
                     <th className="px-6 py-4 font-medium">Company</th>
                     <th className="px-6 py-4 font-medium">Position</th>
+                    <th className="px-6 py-4 font-medium">Location</th>
                     <th className="px-6 py-4 font-medium">Status</th>
+                    <th className="px-6 py-4 font-medium">Employment Type</th>
                     <th className="px-6 py-4 font-medium">Date Applied</th>
                     <th className="px-6 py-4 font-medium">Actions</th>
                 </tr>
@@ -39,25 +43,46 @@ export default function ApplicationTable({
                     </td>
 
                     <td className="px-6 py-4">
+                        {application.location}
+                    </td>
+
+                    <td className="px-6 py-4">
                         {application.status}
                     </td>
 
                     <td className="px-6 py-4">
-                        {application.dateApplied}
+                        {application.employmentType}
                     </td>
 
-                    <td className="flex px-6 py-4 gap-2">
-                        <button 
-                            type="button" 
-                            onClick={() => onEdit(application)}
-                            className="text-green-600 hover:text-green-800">
-                            <LuPencil size={18} />
-                        </button>
-                        <button
-                            type="button"
-                            className="text-red-600 hover:text-red-800">
-                            <LuTrash2 size={18} />
-                        </button>
+                    <td className="px-2 py-4 ">
+                        {formatDate(application.dateApplied)}
+                    </td>
+
+                    <td className="px-6 py-4 ">
+                        <div className="flex gap-2 justify-center">
+                            <button
+                                type="button"
+                                title="View Details"
+                                aria-label="View Details"
+                                className="text-blue-600 hover:text-blue-800">
+                                <LuEye size={18} />
+                            </button>
+                            <button 
+                                type="button" 
+                                title="Edit"
+                                aria-label="Edit"
+                                onClick={() => onEdit(application)}
+                                className="text-green-600 hover:text-green-800">
+                                <LuPencil size={18} />
+                            </button>
+                            <button
+                                type="button"
+                                title="Delete"
+                                aria-label="Delete"
+                                className="text-red-600 hover:text-red-800">
+                                <LuTrash2 size={18} />
+                            </button>
+                        </div>
                     </td>
                     </tr>
                 ))}
